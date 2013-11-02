c30dfd0590a1102464529a089c481271f258c6a4
Pamela Almendares (Casa) <pamelocho@hotmail.com>
2013-11-02 16:54:59 -0600

    probando cositas

--- config/routes.js
+++ config/routes.js
@@ -41,9 +41,9 @@ module.exports.routes = {
   /*
   // But what if you want your home page to display
   // a signup form located at `views/user/signup.ejs`? */
-  '/signup': {
-    controller: 'Signup',
-    action: 'showForm'
+  '/user': {
+    controller: 'UserController',
+    action: 'render'
   }
   /*
 
