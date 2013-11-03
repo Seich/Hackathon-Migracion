c4841b237ee434a2a6e9686f3f4a2f5308808679
Gazy Mahomar <gmahomarf@gmail.com>
2013-11-02 18:24:55 -0600

    Added Dashboard beginnings.

---
+++ api/controllers/DashboardController.js
@@ -0,0 +1,79 @@
+/**
+ * DashboardController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+ module.exports = {
+
+
+  /**
+   * Action blueprints:
+   *    `/dashboard/index`
+   *    `/dashboard`
+   */
+   index: function (req, res) {
+
+    var pendingTransactions = [
+    {
+        transactionDate: "28/09/2013",
+        transactionRecipient: "ENEE",
+        transactionStatus: "Procesado"
+    },
+
+    {
+        transactionDate: "28/10/2013",
+        transactionRecipient: "SANAA",
+        transactionStatus: "Retrasado"
+    },
+
+
+    {
+        transactionDate: "15/10/2013",
+        transactionRecipient: "Hondutel",
+        transactionStatus: "Procesado"
+    }
+
+    ];
+    
+    return res.view('dashboard/index',
+        {
+            pendingTransactions: pendingTransactions,
+            currentBalance: 325.56,
+            firstName: "Pedro",
+            lastName: "Martínez"
+        });
+},
+
+
+  /**
+   * Action blueprints:
+   *    `/dashboard/settings`
+   */
+   settings: function (req, res) {
+
+    return res.view('dashboard/settings');
+},
+
+
+
+
+  /**
+   * Overrides for the settings in `config/controllers.js`
+   * (specific to DashboardController)
+   */
+   _config: {}
+
+
+};
