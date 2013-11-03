5b7b99eaef71cb01fa1c29785e59b0e6db0d3b64
Sergio Díaz <seich@martianwabbit.com>
2013-11-02 20:52:25 -0600

    Working on the new service thing in the remittance.

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,65 +7,28 @@
  * Feel free to change none, some, or ALL of this file to fit your needs!
  */
 
+ $(function() {
+	$('#destination', '#new_remittance').on('change', function() {
 
-(function (io) {
+		var r_services = $.getJSON('/service/find_by_country', {country: $(this).val()});
+		r_services.done(function(services, status) {
+			var $new_service = $('#new_service');
+			$new_service.removeAttr('disabled');
 
-  // as soon as this file is loaded, connect automatically, 
-  var socket = io.connect();
-  if (typeof console !== 'undefined') {
-    log('Connecting to Sails.js...');
-  }
+			$new_service.on('click', function() {
+				var $service = $('.new', "ul.services");
+				$.each(services, function(i, service) {
+					var o = $('<option/>', {
+						'text': service.company,
+						'value': service.company
+					});
 
-  socket.on('connect', function socketConnected() {
+					o.appendTo($service);
+				});
 
-    // Listen for Comet messages from Sails
-    socket.on('message', function messageReceived(message) {
+				return false;
+			});
+		});
 
-      ///////////////////////////////////////////////////////////
-      // Replace the following with your own custom logic
-      // to run when a new message arrives from the Sails.js
-      // server.
-      ///////////////////////////////////////////////////////////
-      log('New comet message received :: ', message);
-      //////////////////////////////////////////////////////
-
-    });
-
-
-    ///////////////////////////////////////////////////////////
-    // Here's where you'll want to add any custom logic for
-    // when the browser establishes its socket connection to 
-    // the Sails.js server.
-    ///////////////////////////////////////////////////////////
-    log(
-        'Socket is now connected and globally accessible as `socket`.\n' + 
-        'e.g. to send a GET request to Sails, try \n' + 
-        '`socket.get("/", function (response) ' +
-        '{ console.log(response); })`'
-    );
-    ///////////////////////////////////////////////////////////
-
-
-  });
-
-
-  // Expose connected `socket` instance globally so that it's easy
-  // to experiment with from the browser console while prototyping.
-  window.socket = socket;
-
-
-  // Simple log function to keep the example simple
-  function log () {
-    if (typeof console !== 'undefined') {
-      console.log.apply(console, arguments);
-    }
-  }
-  
-
-})(
-
-  // In case you're wrapping socket.io to prevent pollution of the global namespace,
-  // you can replace `window.io` with your own `io` here:
-  window.io
-
-);
+	});
+ });
