a4e4a2c9adf89ed9cbd9b80b89febbce2d40c953
Sergio Díaz <seich@martianwabbit.com>
2013-11-02 23:42:04 -0600

    Added ejs to handle complex clientside templates.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,25 +4,22 @@
 		var r_services = $.getJSON('/service/find_by_country', {country: $(this).val()});
 		r_services.done(function(services, status) {
 			var $new_service = $('#new_service');
+			var $service = $('.new_services');
+
 			$new_service.removeAttr('disabled');
 
 			$new_service.on('click', function() {
-				var $service = $('.new_services').show();
-				$service.empty();
-				$.each(services, function(i, service) {
-					var option = $('<li/>', {
-						'text': service.company
-					});
-
-					option.prepend($('<img />', {
-						src: '/images/logos/' + service.logo
-					}));
-
-					option.appendTo($service);
-				});
+				$service.show().empty();
+				$service.append(new EJS({url: '/views/new_remittance_service.ejs'}).render(services));
 
 				return false;
 			});
+
+			$('.new_services li').on('click', function() {
+				
+			});
+
+
 		});
 
 	});
