b8dfb2cd3f52e80606b5b45803979b211b38fc57
Sergio Díaz <seich@martianwabbit.com>
2013-11-03 00:08:14 -0600

    Changing variable names.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,23 +2,30 @@
 	$('#destination', '#new_remittance').on('change', function() {
 
 		var r_services = $.getJSON('/service/find_by_country', {country: $(this).val()});
-		r_services.done(function(services, status) {
-			var $new_service = $('#new_service');
+		var $new_service = $('#new_service');
 			var $service = $('.new_services');
 
+		r_services.done(function(services, status) {
 			$new_service.removeAttr('disabled');
 
 			$new_service.on('click', function() {
 				$service.show().empty();
-				$service.append(new EJS({url: '/views/new_remittance_service.ejs'}).render(services));
+
+				$.each(services, function(i, service) {
+					service.index = i;
+					$service.append(new EJS({url: '/views/new_remittance_service.ejs'}).render(service));
+				})
 
 				return false;
 			});
 
-			$('.new_services li').on('click', function() {
-				
-			});
+			$('.new_services').on('click', 'li', function() {
+				var $this = $(this);
+				var i = $this.attr('data-id');
+				console.log(i)
+				$service.append(new EJS({url: '/views/new_remittance_service.ejs'}).render(services[i]));
 
+			});
 
 		});
 
